25e3735a4d4532e6421fcfe4ed3b65c11ea22ef3
Guodong Xu <guodong.xu@linaro.org>
2015-04-09 16:45:29 +0800

    backports: remove unused macros from if.h. Later kernel have it defined.
    Signed-off-by: Guodong Xu <guodong.xu@linaro.org>

--- backport/backport-include/linux/if.h
+++ backport/backport-include/linux/if.h
@@ -3,11 +3,6 @@
 #include_next <linux/if.h>
 #include <linux/version.h>
 
-/* mask IFF_DONT_BRIDGE as RHEL6 backports this */
-#if !defined(IFF_DONT_BRIDGE)
-#define IFF_DONT_BRIDGE 0x800		/* disallow bridging this ether dev */
-#endif
-
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,36))
 #define br_port_exists(dev)	(dev->br_port)
 #else
@@ -23,22 +18,6 @@
  * edit the upstrema code for backport efforts.
  */
 #define br_port_exists(dev)	(dev->priv_flags & IFF_BRIDGE_PORT)
-#endif
-
-#ifndef  IFF_TX_SKB_SHARING
-#define IFF_TX_SKB_SHARING	0x10000
-#endif
-
-#ifndef IFF_LIVE_ADDR_CHANGE
-#define IFF_LIVE_ADDR_CHANGE 0x100000
-#endif
-
-#ifndef IFF_SUPP_NOFCS
-#define IFF_SUPP_NOFCS	0x80000		/* device supports sending custom FCS */
-#endif
-
-#ifndef IFF_UNICAST_FLT
-#define IFF_UNICAST_FLT	0x20000		/* Supports unicast filtering	*/
 #endif
 
 #endif	/* _BACKPORT_LINUX_IF_H */
